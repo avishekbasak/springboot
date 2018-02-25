1105d2470add967c1bf904ce286e4584b08ccffc
Avishek Basak <avishek.basak1@gmail.com>
2018-02-25 18:12:03 -0500

    Add fronend domain model for basic

--- src/test/java/com/devopsbuddy/test/unit/UserUtilsUnitTest.java
+++ src/test/java/com/devopsbuddy/test/unit/UserUtilsUnitTest.java
@@ -1,28 +1,29 @@
 package com.devopsbuddy.test.unit;
 
-import com.devopsbuddy.backend.persistence.domain.backend.User;
-import com.devopsbuddy.utils.UserUtils;
-import com.devopsbuddy.web.controllers.ForgotMyPasswordController;
-//import com.devopsbuddy.web.domain.frontend.BasicAccountPayload;
+import java.util.UUID;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
-//import uk.co.jemos.podam.api.PodamFactory;
-//import uk.co.jemos.podam.api.PodamFactoryImpl;
 
-import java.util.UUID;
+import com.devopsbuddy.backend.persistence.domain.backend.User;
+import com.devopsbuddy.utils.UserUtils;
+import com.devopsbuddy.web.controllers.ForgotMyPasswordController;
+import com.devopsbuddy.web.domain.frontend.BasicAccountPayload;
+
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
 
 public class UserUtilsUnitTest {
 
     private MockHttpServletRequest mockHttpServletRequest;
 
-    //private PodamFactory podamFactory;
+    private PodamFactory podamFactory;
 
     @Before
     public void init() {
         mockHttpServletRequest = new MockHttpServletRequest();
-        //podamFactory = new PodamFactoryImpl();
+        podamFactory = new PodamFactoryImpl();
     }
 
     @Test
@@ -42,23 +43,23 @@ public class UserUtilsUnitTest {
 
     }
 
-//    @Test
-//    public void mapWebUserToDomainUser() {
-//
-//        BasicAccountPayload webUser = podamFactory.manufacturePojoWithFullData(BasicAccountPayload.class);
-//        webUser.setEmail("me@example.com");
-//
-//        User user = UserUtils.fromWebUserToDomainUser(webUser);
-//        Assert.assertNotNull(user);
-//
-//        Assert.assertEquals(webUser.getUsername(), user.getUsername());
-//        Assert.assertEquals(webUser.getPassword(), user.getPassword());
-//        Assert.assertEquals(webUser.getFirstName(), user.getFirstName());
-//        Assert.assertEquals(webUser.getLastName(), user.getLastName());
-//        Assert.assertEquals(webUser.getEmail(), user.getEmail());
-//        Assert.assertEquals(webUser.getPhoneNumber(), user.getPhoneNumber());
-//        Assert.assertEquals(webUser.getCountry(), user.getCountry());
-//        Assert.assertEquals(webUser.getDescription(), user.getDescription());
-//
-//    }
+    @Test
+    public void mapWebUserToDomainUser() {
+
+        BasicAccountPayload webUser = podamFactory.manufacturePojoWithFullData(BasicAccountPayload.class);
+        webUser.setEmail("me@example.com");
+
+        User user = UserUtils.fromWebUserToDomainUser(webUser);
+        Assert.assertNotNull(user);
+
+        Assert.assertEquals(webUser.getUsername(), user.getUsername());
+        Assert.assertEquals(webUser.getPassword(), user.getPassword());
+        Assert.assertEquals(webUser.getFirstName(), user.getFirstName());
+        Assert.assertEquals(webUser.getLastName(), user.getLastName());
+        Assert.assertEquals(webUser.getEmail(), user.getEmail());
+        Assert.assertEquals(webUser.getPhoneNumber(), user.getPhoneNumber());
+        Assert.assertEquals(webUser.getCountry(), user.getCountry());
+        Assert.assertEquals(webUser.getDescription(), user.getDescription());
+
+    }
 }
